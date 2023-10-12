c3f7afbeb85ee287a9fb274b9a1c29320c127804
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-12 05:31:24 -0700

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,20 +1,27 @@
 -- a SQL script that creates a stored procedure ComputeAverageScoreForUser
 --that computes and store the average score for a student
 DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(5, 2);
+    DECLARE total_score FLOAT;
+    DECLARE total_corrections INT;
+    DECLARE avg FLOAT;
 
-    -- Compute the average score
-    SELECT AVG(score) INTO avg_score
+    -- Calculate the total score and total number of corrections for the user
+    SELECT SUM(score), COUNT(*) INTO total_score, total_corrections
     FROM corrections
     WHERE user_id = user_id;
 
-    -- Update the user's average score
+    -- Calculate the average score, or set to 0 if there are no corrections
+    IF total_corrections > 0 THEN
+        SET avg = total_score / total_corrections;
+    ELSE
+        SET avg = 0;
+    END IF;
+
+    -- Update the user's average_score in the users table
     UPDATE users
-    SET average_score = avg_score
+    SET average_score = avg
     WHERE id = user_id;
 END;
 //
