661f6611912d2f69ec7e6d68d56bf6fba25c7bb6
MokiEng <meconen20@gmail.com>
2023-10-12 12:15:15 +0000

    trigger 1

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- Create a trigger to reset valid_email when email is changed
+DELIMITER //
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON your_table -- Replace "your_table" with the actual table name
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email THEN
+        SET NEW.valid_email = 0; -- Set valid_email to 0 when email changes
+    END IF;
+END;
+//
+DELIMITER ;
