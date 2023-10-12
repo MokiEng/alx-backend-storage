b4e5871abda175aa5a9cf0aefeb44a39c8456732
MokiEng <meconen20@gmail.com>
2023-10-12 13:29:43 +0000

    average weight

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,2 +1,25 @@
 -- a SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser
 --  that computes and store the average weighted score for a student.
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE total_score DECIMAL(5, 2);
+    DECLARE total_weight DECIMAL(5, 2);
+
+    SELECT SUM(score * weight) INTO total_score, SUM(weight) INTO total_weight
+    FROM corrections
+    WHERE user_id = user_id;
+
+    IF total_weight IS NOT NULL THEN
+        SET total_score = total_score / total_weight;
+    ELSE
+        SET total_score = 0;
+    END IF;
+
+    UPDATE users
+    SET average_weighted_score = total_score
+    WHERE id = user_id;
+END $$
+
+DELIMITER ;
