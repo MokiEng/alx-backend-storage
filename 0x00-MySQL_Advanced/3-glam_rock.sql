f49cbcfbee87584f18a82789dfa20dbc01cb318e
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-12 11:11:54 -0700

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
--- SQL script that lists all bands with Glam rock as
---  their main style, ranked by their longevity.
+-- a SQL script that lists all bands with Glam rock as
+-- their main style, ranked by their longevity.
 SELECT
   name AS band_name,
   2022 - CAST(SUBSTRING_INDEX(formed, '-', 1) AS SIGNED) AS lifespan
