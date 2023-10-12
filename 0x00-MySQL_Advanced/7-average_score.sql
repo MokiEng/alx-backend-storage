183f72e43cf7196f2774b9e461f6cebcdc85c589
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-12 05:32:57 -0700

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,28 +1,22 @@
 -- a SQL script that creates a stored procedure ComputeAverageScoreForUser
---that computes and store the average score for a student
-DELIMITER //
-CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE total_corrections INT;
-    DECLARE avg FLOAT;
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
 
-    -- Calculate the total score and total number of corrections for the user
-    SELECT SUM(score), COUNT(*) INTO total_score, total_corrections
-    FROM corrections
-    WHERE user_id = user_id;
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
 
-    -- Calculate the average score, or set to 0 if there are no corrections
-    IF total_corrections > 0 THEN
-        SET avg = total_score / total_corrections;
-    ELSE
-        SET avg = 0;
-    END IF;
-
-    -- Update the user's average_score in the users table
     UPDATE users
-    SET average_score = avg
-    WHERE id = user_id;
-END;
-//
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
+END $$
 DELIMITER ;
