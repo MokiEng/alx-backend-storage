7185eab8e2387e308956936f87e51b42dc3b9ffe
MokiEng <meconen20@gmail.com>
2023-10-12 13:01:30 +0000

    div

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,16 @@
+--a SQL script that creates a function SafeDiv that divides (and returns) the first by the second number
+DELIMITER //
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    ELSE
+        RETURN a / b;
+    END IF;
+END;
+//
+
+DELIMITER ;
