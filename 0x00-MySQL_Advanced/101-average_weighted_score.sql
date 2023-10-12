2e4dbc2f4d0a9019347977276e0ab62a70a62f30
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-12 09:12:16 -0700

    Update 101-average_weighted_score.sql

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,67 +1,36 @@
--- a SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers
--- that computes and store the average weighted score for all students.
-DELIMITER $$
-
+-- To create a stored procedure named ComputeAverageWeightedScoreForUsers
+-- that calculates and stores the average weighted score for all students
+DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE done INT DEFAULT FALSE;
-    DECLARE user_id INT;
+    DECLARE total_weighted_score FLOAT;
+    DECLARE total_weight FLOAT;
 
-    -- Declare a cursor for selecting distinct user IDs
-    DECLARE cur CURSOR FOR SELECT DISTINCT user_id FROM corrections;
+    -- Initialize variables
+    SET total_weighted_score = 0;
+    SET total_weight = 0;
 
-    -- Declare variables to store the computed values
-    DECLARE total_score DECIMAL(5, 2);
-    DECLARE total_weight DECIMAL(5, 2);
-
-    -- Create a temporary table to store the results
-    CREATE TEMPORARY TABLE IF NOT EXISTS temp_average_weighted_score (user_id INT, average_score DECIMAL(5, 2));
-
-    -- Open the cursor
-    OPEN cur;
-
-    -- Start processing rows
-    read_loop: LOOP
-        FETCH cur INTO user_id;
-
-        IF done THEN
-            LEAVE read_loop;
-        END IF;
-
-        -- Calculate the total weighted score
-        SELECT SUM(c.score * p.weight) INTO total_score
-        FROM corrections AS c
-        JOIN projects AS p ON c.project_id = p.id
-        WHERE c.user_id = user_id;
-
-        -- Calculate the total weight
-        SELECT SUM(p.weight) INTO total_weight
-        FROM corrections AS c
-        JOIN projects AS p ON c.project_id = p.id
-        WHERE c.user_id = user_id;
-
-        -- Calculate the average weighted score
-        IF total_weight IS NOT NULL THEN
-            SET total_score = total_score / total_weight;
-        ELSE
-            SET total_score = 0;
-        END IF;
-
-        -- Insert the computed result into the temporary table
-        INSERT INTO temp_average_weighted_score (user_id, average_score) VALUES (user_id, total_score);
-
-    END LOOP;
-
-    -- Close the cursor
-    CLOSE cur;
-
-    -- Update the users table with the computed average scores
+    -- Calculate the weighted score for each student
     UPDATE users
-    JOIN temp_average_weighted_score AS t ON users.id = t.user_id
-    SET users.average_score = t.average_score;
+    SET average_score = (
+        SELECT SUM(score * weight) / SUM(weight)
+        FROM corrections
+        JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = users.id
+    );
 
-    -- Drop the temporary table
-    DROP TEMPORARY TABLE IF EXISTS temp_average_weighted_score;
-END $$
+    -- Calculate the total weighted score for all students
+    SELECT SUM(average_score * weight) INTO total_weighted_score
+    FROM users
+    JOIN corrections ON users.id = corrections.user_id
+    JOIN projects ON corrections.project_id = projects.id;
 
+    -- Calculate the total weight for all students
+    SELECT SUM(weight) INTO total_weight
+    FROM projects;
+
+    -- Update the average_score for all students
+    UPDATE users
+    SET average_score = total_weighted_score / total_weight;
+END //
 DELIMITER ;
