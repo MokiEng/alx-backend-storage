d2c42c798e6eb7cb7b91598ec4b98c930f29b580
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-12 06:07:43 -0700

    Update 10-div.sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,16 +1,14 @@
 --a SQL script that creates a function SafeDiv that divides (and returns) the first by the second number
-DELIMITER //
-
-CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT
-DETERMINISTIC
+DROP FUNCTION IF EXISTS SafeDiv;
+DELIMITER $$
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
-    IF b = 0 THEN
-        RETURN 0;
-    ELSE
-        RETURN a / b;
-    END IF;
-END;
-//
+    DECLARE result FLOAT DEFAULT 0;
 
+    IF b != 0 THEN
+        SET result = a / b;
+    END IF;
+    RETURN result;
+END $$
 DELIMITER ;
