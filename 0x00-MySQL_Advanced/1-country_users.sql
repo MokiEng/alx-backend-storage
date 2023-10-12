76c7082a0634e16c7828dfde3cc5d5c9a781587f
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-12 04:39:46 -0700

    Update 1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
---create the "users" table with the specified attributes and constraints.
+-- Check if the table already exists, and create it only if it doesn't
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
