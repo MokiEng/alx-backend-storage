331d4982df1e2a1dab5a973a4529a6092f625fc0
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-18 10:59:12 -0700

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,33 +6,44 @@ particular URL and returns it.
 import requests
 import redis
 from functools import wraps
+from typing import Callable
+from datetime import datetime, timedelta
+
+redis_client = redis.Redis()
 
 
-def cache_page(fn):
-    """a decorator cache_page"""
-    @wraps(fn)
-    def decorated(*args, **kwargs):
-        url = args[0]
-        cache_key = f'count:{url}'
-        cached_result = cache.get(cache_key)
-
-        if cached_result:
-            cache.incr(cache_key)
-            return cached_result
-
-        response = fn(*args, **kwargs)
-        cache.setex(cache_key, 10, response)
-        return response
-    return decorated
-
-
-cache = redis.Redis(host='localhost', port=6379, db=0)
-
-
-@cache_page
-def get_page(url):
-    """automatically cache the result and track the
-    number of times the URL was accessed.
-    """
+def get_page(url: str) -> str:
+    """ Check if the URL is cached."""
+    cached_result = redis_client.get(url)
+    if cached_result:
+        return cached_result.decode('utf-8')
     response = requests.get(url)
-    return response.text
+    if response.status_code == 200:
+        content = response.text
+    else:
+        content = f"Failed to retrieve the content from {url}"
+    redis_client.setex(url, timedelta(seconds=10), content)
+    increment_url_count(url)
+    return content
+
+def increment_url_count(url: str):
+    """Track the number of times the URL is accessed"""
+    count_key = f'count:{url}'
+    current_count = redis_client.get(count_key)
+    if current_count:
+        new_count = int(current_count) + 1
+    else:
+        new_count = 1
+    redis_client.set(count_key, new_count)
+
+def cache_result(fn):
+    @wraps(fn)
+    def wrapper(url):
+        cached_result = redis_client.get(url)
+        if cached_result:
+            return cached_result.decode('utf-8')
+        result = fn(url)
+        redis_client.setex(url, timedelta(seconds=10), result)
+        return result
+    return wrapper
+get_page = cache_result(get_page)
