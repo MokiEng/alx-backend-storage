fc94d9e465d5fda6999dcf59ffd4ffe73df4d451
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-10-18 11:25:26 -0700

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,38 +7,15 @@ import requests
 import redis
 from functools import wraps
 from typing import Callable
-from datetime import datetime, timedelta
 
 redis_client = redis.Redis()
+"""The module-level Redis instance."""
 
 
-def get_page(url: str) -> str:
-    """ Check if the URL is cached."""
-    cached_result = redis_client.get(url)
-    if cached_result:
-        return cached_result.decode('utf-8')
-    response = requests.get(url)
-    if response.status_code == 200:
-        content = response.text
-    else:
-        content = f"Failed to retrieve the content from {url}"
-    redis_client.setex(url, timedelta(seconds=10), content)
-    increment_url_count(url)
-    return content
-
-def increment_url_count(url: str):
-    """Track the number of times the URL is accessed"""
-    count_key = f'count:{url}'
-    current_count = redis_client.get(count_key)
-    if current_count:
-        new_count = int(current_count) + 1
-    else:
-        new_count = 1
-    redis_client.set(count_key, new_count)
-
-def cache_result(fn):
-    @wraps(fn)
-    def wrapper(url):
+def data_cacher(method: Callable) -> Callable:
+    """Caches the output of fetched data."""
+    @wraps(method)
+    def wrapper(url) -> str:
         cached_result = redis_client.get(url)
         if cached_result:
             return cached_result.decode('utf-8')
@@ -46,4 +23,9 @@ def cache_result(fn):
         redis_client.setex(url, timedelta(seconds=10), result)
         return result
     return wrapper
-get_page = cache_result(get_page)
+
+
+@data_cacher
+def get_page(url: str) -> str:
+    """ Check if the URL is cached."""
+    return requests.get(url).text
